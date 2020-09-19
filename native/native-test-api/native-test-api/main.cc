a48d220addb9e44524127568030c1ddcaf02ce11
Brad Carter <carterbs89@gmail.com>
2020-09-19 19:53:11 -0400

    Add files

--- native/native-test-api/native-test-api/main.cc
+++ native/native-test-api/native-test-api/main.cc
@@ -15,14 +15,12 @@ Napi::Array getMonitorInfo(Napi::Env env) {
     CGError cgret;
     uint32_t num_displays = 0;
     
-    cgret = CGGetActiveDisplayList(0, NULL, &num_displays);
     std::vector<CGDirectDisplayID> displays(num_displays);
     cgret = CGGetActiveDisplayList(num_displays, displays.begin().base(), &num_displays);
     Napi::Array returnValue = Napi::Array::New(env, num_displays);
     
     for(uint32_t d=0; d<num_displays; d++) {
-        CGSize size = CGDisplayScreenSize(displays[d]);
-        returnValue[d] = "{\"deviceId\":\"" + std::to_string(displays[d]) + "\",\"width\":\"" + std::to_string(size.width) + "\",\"height\":\"" + std::to_string(size.height) + "\"}";
+        returnValue[d] = "{\"deviceId\":\"" + std::to_string(displays[d]) + "\",\"width\":\"" + std::to_string(CGDisplayPixelsWide(displays[d])) + "\",\"height\":\"" + std::to_string(CGDisplayPixelsHigh(displays[d])) + "\"}";
     }
     return returnValue;
 }
